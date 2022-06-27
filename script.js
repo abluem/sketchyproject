f5d9dba5b9d0a9619b74210df8256f1f023652e3
acbluem <acbluem@gmail.com>
2022-06-27 04:58:53 -0400

    changed color scheme

--- script.js
+++ script.js
@@ -5,28 +5,33 @@ const resetBoxes = document.querySelector('#resetbutton');
 square.style.background = '#fff';
 
 window.addEventListener('load', function() {
-    for (i = 0; i < 256; i++) {
-        cloneSquare();
-    }
+  for (i = 0; i < 256; i++) {
+    cloneSquare();
+  }
 });
 
 box.addEventListener('mouseover', function(e) {
-    e.target.style.background = '#000';
+  e.target.style.background = 'linear-gradient(red,orange,yellow,green,blue,indigo,violet)';
 });
 
 resetBoxes.addEventListener('click', function() {
-    let numBoxes = prompt('How many squares per side do you want? Please type a number between 1-500.', '');
-    box.style.setProperty('grid-template', `repeat(${numBoxes}, 1fr) / repeat(${numBoxes}, 1fr)`);
-    if (numBoxes > 0 && numBoxes < 501) {
-        while (box.firstChild) {
-            box.removeChild(box.firstChild);
-        }
-        let boxDimensions = numBoxes * numBoxes;
-        for (i = 0; i < boxDimensions; i++) {
-            cloneSquare();
-        }
+  let numBoxes = prompt('How many squares per side do you want? Please type a number between 1-100.', '');
+
+  box.style.setProperty('grid-template', `repeat(${numBoxes}, 1fr) / repeat(${numBoxes}, 1fr)`);
+
+  if (numBoxes > 0 && numBoxes < 101) {
+    while (box.firstChild) {
+      box.removeChild(box.firstChild);
+    }
+
+    let boxDimensions = numBoxes * numBoxes;
+
+    for (i = 0; i < boxDimensions; i++) {
+      cloneSquare();
+    }
+
     } else {
-        alert('Sorry, please enter a number between 1-500.');
+        alert('Sorry, please enter a number between 1-100 next time.');
     }
 });
 
